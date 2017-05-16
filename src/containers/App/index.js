774bec112053762bb4f8b931452df4de9bcbb757
Gabriel West <gabe_west1@yahoo.com>
2017-05-16 12:58:36 -0500

    Added semantic-ui-icons css

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom"
 
 import Container from "./Container"
 import Home from "../Home"
+import "semantic-ui-icon/icon.min.css"
 
 class App extends React.Component{
     render() {
