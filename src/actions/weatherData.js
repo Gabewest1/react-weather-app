a6e3d254a18fc84cfc04caf14eb78b9e33d3c72f
Gabriel West <gabe_west1@yahoo.com>
2017-05-15 19:22:30 -0500

    Added action to show or hide the weather details

--- src/actions/weatherData.js
+++ src/actions/weatherData.js
@@ -3,8 +3,13 @@ import {
     FETCH_WEATHER_DATA,
     FETCH_WEATHER_DATA_SUCCESS,
     FETCH_WEATHER_DATA_ERROR,
+    SHOW_WEATHER_DATA,
 } from "../constants"
 
 export function fetchWeatherData(location) {
     return { type: FETCH_WEATHER_DATA, location }
+}
+
+export function showWeatherData(bool) {
+    return { type: SHOW_WEATHER_DATA, payload: bool }
 }
