bca936c2fb009e593a2bbeb11984daf040d7f142
Gabriel West <gabe_west1@yahoo.com>
2017-05-17 23:06:53 -0500

    Removed code that was moved into WeatherForm

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -10,42 +10,6 @@ import WeatherForm from "../WeatherForm"
 import * as actions from "../WeatherForm/actions"
 
 class Home extends React.Component {
-    handleSubmit(values) {
-        let { location } = values
-        let showData = this.props.weatherData.get("showData")
-
-        //if the weather has already been previously queried and is currently being displayed,
-        //then hide the data so the user can make another query. Otherwise, query for the new weather 
-        //data
-        if(showData) {
-            this.props.showWeatherData(false)
-        } else {
-            this.props.fetchWeatherData(location)
-        }
-    }
-    renderWeatherData() {
-        let { 
-            temperature,
-            timezone,
-            humidity,
-            windSpeed,
-            rain,
-            summary,
-            icon 
-        } = this.props
-        return (
-            <ul>
-                <li>Summary: {summary} </li>
-                <li>Timezone: {timezone} </li>
-                <li>Temperature: {temperature} </li>
-                <li>Humidity: {humidity} </li>
-                <li>Wind Speed: {windSpeed} </li>
-                <li>Chance of Rain: {rain} </li>
-                <li>Icon to display: {icon} </li>
-            </ul>
-        )
-    }
-
     render() {
         return (
             <Container>
