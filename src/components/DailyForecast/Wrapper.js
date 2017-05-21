6d6f6e8a6b511d27ecf45c57846b46ddf4c72f5f
Gabriel West <gabe_west1@yahoo.com>
2017-05-20 23:33:12 -0500

    updated css

--- src/components/DailyForecast/Wrapper.js
+++ src/components/DailyForecast/Wrapper.js
@@ -1,13 +1,17 @@
 import styled from "styled-components"
-
+import { primary, tertiary } from "../../theme/colors"
+console.log("tertiary:", tertiary)
 export default styled.div`
-    background-color: #272A40;
+    background-color: ${primary};
     display: flex;
     flex-direction: column;
     align-items: center;
-    border: solid 5px #A65B0B;
+    border: solid 5px ${tertiary};
+    width: 10vw;
     h1 {
         font-size: 1.5vw;
-
     }
+    // img {
+    //     flex: 0 0 30%;
+    // }
 `
