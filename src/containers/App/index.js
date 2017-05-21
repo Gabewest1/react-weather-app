b2eac0cf7b1893edf5125d2a83f3d75510d74617
Gabriel West <gabe_west1@yahoo.com>
2017-05-20 23:35:09 -0500

    Removed background-image and replaced with an iframe

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -9,6 +9,7 @@ class App extends React.Component{
     render() {
         return (
             <Container>
+                <iframe id="bgFrame" width="560" height="315" src="https://www.youtube.com/embed/yamiiGk6aSs?enablejsapi=1&autoplay=1&controls=0&showinfo=0&" frameborder="0" allowfullscreen></iframe>
                 <Route path="/" component={Home} />
             </Container>
         )
