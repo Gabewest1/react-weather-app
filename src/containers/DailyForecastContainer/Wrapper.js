bcecb92b65fd0931b49b9e65dff010ce1910222a
Gabriel West <gabe_west1@yahoo.com>
2017-06-26 21:28:56 -0500

    Updated styles for `DailyForecastContainer` by removing `flex` and `overflow`

--- src/containers/DailyForecastContainer/Wrapper.js
+++ src/containers/DailyForecastContainer/Wrapper.js
@@ -2,6 +2,6 @@ import styled from "styled-components"
 
 export default styled.div`
     display: flex;
-    flex: 1 1 ${(100/7)}%;
-    overflow-x: auto;
+    justify-content: space-between;
+    align-items: center;
 `
