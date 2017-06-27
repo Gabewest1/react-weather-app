f31942b0c64f70d5d13850d0710c6337d61dc94d
Gabriel West <gabe_west1@yahoo.com>
2017-06-26 21:26:14 -0500

    Changed `DailyForecast` styles to not have a background-color, border, or width explicitly defined

--- src/components/DailyForecast/Wrapper.js
+++ src/components/DailyForecast/Wrapper.js
@@ -1,17 +1,13 @@
 import styled from "styled-components"
 import { primary, tertiary } from "../../theme/colors"
-console.log("tertiary:", tertiary)
+
 export default styled.div`
-    background-color: ${primary};
     display: flex;
     flex-direction: column;
     align-items: center;
-    border: solid 5px ${tertiary};
-    width: 10vw;
+    justify-content: center;
+
     h1 {
         font-size: 1.5vw;
     }
-    // img {
-    //     flex: 0 0 30%;
-    // }
 `
