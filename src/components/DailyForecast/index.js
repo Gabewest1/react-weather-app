213cd04917f0fc9054d96ffd7b8fe9313414ddbc
Gabriel West <gabe_west1@yahoo.com>
2017-07-19 23:49:59 -0500

    Centered text and added a divider between hi and lo temperatures

--- src/components/DailyForecast/index.js
+++ src/components/DailyForecast/index.js
@@ -2,15 +2,16 @@ import React from "react"
 import Wrapper from "./Wrapper"
 import WeatherIcon from "../WeatherIcon"
 
+
 export default (props) => {
     let { icon, precipProbability, maxTemp, minTemp, summary } = props
 
     return (
         <Wrapper>
             <WeatherIcon icon={icon} />
-            <h1>
+            <h1 style={{textAlign: "center"}}>
                 <b>{maxTemp}&deg;f</b> 
-                <br /> 
+                <div style={{width: "100%", height: "1px", background: "white"}}/>
                 {minTemp}&deg;f
             </h1>
         </Wrapper>
