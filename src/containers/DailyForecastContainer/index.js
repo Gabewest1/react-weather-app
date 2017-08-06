5f787333e40ab88c00f1e78b6f88a9dce41e80c5
Gabriel West <gabe_west1@yahoo.com>
2017-08-05 20:53:35 -0500

    Convert max and min temperature to celcius if celciusMode is true

--- src/containers/DailyForecastContainer/index.js
+++ src/containers/DailyForecastContainer/index.js
@@ -6,10 +6,13 @@ import DailyForecast from "../../components/DailyForecast"
 import Wrapper from "./Wrapper"
 
 import * as weatherDataSelectors from "../WeatherForm/selectors"
+import convertFahrenheitToCelcius from "../../lib/convertFahrenheitToCelcius"
+
+
 
 class DailyForecastContainer extends React.Component {
     createDailyForecasts() {
-        let weeklyForecastData = this.props.weeklyForecastData
+        let { celciusMode, weeklyForecastData, tempSymbol } = this.props
 
         let forecasts =
              weeklyForecastData
@@ -17,8 +20,15 @@ class DailyForecastContainer extends React.Component {
                 .map((dailyForecastData, key) => {
                     let { summary, icon, precipProbability, rain, apparentTemperatureMax, apparentTemperatureMin } = dailyForecastData
 
-                    let day = weatherDataSelectors.selectDayOfTheWeek(key)
-                    let dailyForecastProps = { day, summary, icon, precipProbability, rain, maxTemp:apparentTemperatureMax, minTemp:apparentTemperatureMin }
+                    if (celciusMode) {
+                        apparentTemperatureMax = convertFahrenheitToCelcius(apparentTemperatureMax)
+                        apparentTemperatureMin = convertFahrenheitToCelcius(apparentTemperatureMin)
+                    }
+
+                    console.log("MAX, MIN:", apparentTemperatureMax, apparentTemperatureMin)
+
+                    let day = weatherDataSelectors.selectDayOfTheWeek(key) 
+                    let dailyForecastProps = { day, summary, tempSymbol, icon, precipProbability, rain, maxTemp:apparentTemperatureMax, minTemp:apparentTemperatureMin }
 
                     return (
                         <DailyForecast key={key} {...dailyForecastProps} />
@@ -40,27 +50,12 @@ class DailyForecastContainer extends React.Component {
 function mapStateToProps(state) {
     let data = state.weatherData.get("data")
 
-    //If no data has been fetched from the weather API 
-    //then return false and dont run the select functions
-    let temperature = data ? weatherDataSelectors.selectTemperature(data) : false
-    let timezone = data ? weatherDataSelectors.selectTimezone(data) : false
-    let humidity = data ? weatherDataSelectors.selectHumidity(data) : false
-    let windSpeed = data ? weatherDataSelectors.selectWindSpeed(data) : false
-    let rain = data ? weatherDataSelectors.selectRainProbability(data) : false
-    let summary = data ? weatherDataSelectors.selectSummary(data) : false
-    let icon = data ? weatherDataSelectors.selectIcon(data) : false
     let weeklyForecastData = data ? weatherDataSelectors.selectWeeklyForecastData(data) : false
 
     return {
-        weatherData: state.weatherData,
-        temperature,
-        timezone,
-        humidity,
-        windSpeed,
-        rain,
-        summary,
-        icon,
-        weeklyForecastData
+        weeklyForecastData,
+        tempSymbol: weatherDataSelectors.selectTempSymbol(state),
+        celciusMode: weatherDataSelectors.selectCelciusMode(state)
     }
 }
 
