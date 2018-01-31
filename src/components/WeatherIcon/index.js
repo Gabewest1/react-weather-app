190296dc52ea0f415fb7c376f51d776224de342a
Gabriel West <gabe_west1@yahoo.com>
2018-01-31 00:59:22 -0600

    Created component for the weather icons

--- src/components/WeatherIcon/index.js
+++ src/components/WeatherIcon/index.js
@@ -1,15 +1,43 @@
 import React from "react"
 import styled from "styled-components"
+import ClearDay from "./ClearDay"
+import ClearNight from "./ClearNight"
+import Cloudy from "./Cloudy"
+import Fog from "./Fog"
+import PartlyCloudyDay from "./PartlyCloudyDay"
+import PartlyCloudyNight from "./PartlyCloudyNight"
+import Rain from "./Rain"
+import Snow from "./Snow"
+import Wind from "./Wind"
 
-const WeatherIcon = styled.img`
-    max-width: 100%
-    width: 100%;
-`
+export default ({ icon, tempSymbol, style }) => {    
+    const WeatherIcon = WeatherIconFactory(icon)
 
-export default ({ icon, tempSymbol, style }) => {
-    let iconSrc = `/assets/images/temp/${icon}.svg`
+    return <WeatherIcon style={{ maxWidth: "100%" }} />
+}
 
-    return (
-        <WeatherIcon src={iconSrc} alt={icon} style={ style }/>
-    )
+
+function WeatherIconFactory(type) {
+    switch (type) {
+        case "clear-day":
+            return ClearDay
+        case "clear-night":
+            return ClearNight
+        case "cloudy":
+            return Cloudy
+        case "fog":
+            return Fog
+        case "partly-cloudy-day":
+            return PartlyCloudyDay
+        case "partly-cloudy-night":
+            return PartlyCloudyNight
+        case "rain":
+            return Rain
+        case "snow":
+            return Snow
+        case "wind":
+            return Wind
+        default:
+            return Wind
+    }
 }
