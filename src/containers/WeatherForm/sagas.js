d7bc017a864a151ff9af4c8126b08a64c697017e
Gabriel West <gabe_west1@yahoo.com>
2018-05-21 19:08:36 -0500

    Removed logs from sagas

--- src/containers/WeatherForm/sagas.js
+++ src/containers/WeatherForm/sagas.js
@@ -14,20 +14,16 @@ import {
 } from "./constants"
 
 export function* watchFetchWeatherData() {
-    console.log("watchFetchWeatherData")
     yield takeEvery(FETCH_WEATHER_DATA, fetchLocationData)
 }
 export function* watchFetchCurrentLocationWeatherData() {
-    console.log("watchFetchCurrentLocationWeatherData")
     yield takeEvery(FETCH_CURRENT_LOCATION_WEATHER_DATA, fetchCurrentLocationWeatherData)
 }
 export function* watchFormHeightAnimationStart() {
-    console.log("watchFormHeightAnimationStart")
     yield takeEvery(ANIMATE_FORM_HEIGHT_START, startFormHeightAnimation)
 }
 
 export function* startFormHeightAnimation(action) {
-    console.log("startFormHeightAnimation")
     let state = yield select()
     let collapsed = state.weatherData.get("collapsed")
     
@@ -50,7 +46,6 @@ export function* startFormHeightAnimation(action) {
 
 }
 export function* endFormHeightAnimation(action) {
-    console.log("endFormHeightAnimation")
     const state = yield select()
     const collapsed = state.weatherData.get("collapsed")
     const data = state.weatherData.get("data")
@@ -59,15 +54,12 @@ export function* endFormHeightAnimation(action) {
         yield put({ type: SHOW_WEATHER_DATA, payload: true })
     } else if(collapsed && data) {
         yield startFormHeightAnimation()
-        console.log("Form just collapsed and is re-opening")
         yield take(ANIMATE_FORM_HEIGHT_END)
-        console.log("Form has reopened!!! :DDDDDDDD")
         yield put({ type: SHOW_WEATHER_DATA, payload: true })
     }
 
 }
 export function* getAddressOfCoordinates(lat, lng) {
-    console.log("getAddressOfCoordinates")
     try {
         const apiKey = "AIzaSyBlEuzRfwGV7IIIpUtefZWzHTg5Ip6UO3E"
         const googleGeocoderApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat+","+lng}&key=${apiKey}`
@@ -75,9 +67,7 @@ export function* getAddressOfCoordinates(lat, lng) {
         let response = yield call(fetch, googleGeocoderApiUrl)
         let locationInfo = yield response.json()
 
-        console.log(locationInfo)
         let address = locationInfo.results[0].formatted_address
-        console.log("address", address)
 
         return address
     } catch(e) {
@@ -86,21 +76,16 @@ export function* getAddressOfCoordinates(lat, lng) {
 
 }
 export function* fetchCurrentLocationWeatherData() {
-    console.log("fetchCurrentLocationWeatherData")
     try {
         let position = yield new Promise(resolve => {
-            console.log("About to find your location!", navigator.geolocation.getCurrentPosition)
             navigator.geolocation.getCurrentPosition(position => {
-                console.log("Got the position:", position)
                 resolve(position)
             }, (err) => console.log("GEOCODE ERROR:", err),
-                {timeout: 10000, enableHighAccuracy: true}
+               {timeout: 10000, enableHighAccuracy: true}
             )
         })
         
         let {latitude, longitude} = position.coords
-
-        console.log("Here are the coordinates:", latitude, longitude)
         
         yield fetchWeatherData(latitude, longitude)
     } catch(e) {
@@ -116,7 +101,7 @@ export function* fetchLocationData(action) {
         const locationInfoResponse = yield call(fetch, googleGeocoderApiUrl)
         const locationInfo = yield locationInfoResponse.json()
         const { lat, lng } = locationInfo.results[0].geometry.location
-        console.log(locationInfo)
+
         yield fetchWeatherData(lat, lng)
     } catch(e) {
         console.log(e)
@@ -132,15 +117,12 @@ export function* fetchWeatherData(lat, lng) {
         let data = yield response.json()
         
         let address = yield getAddressOfCoordinates(lat, lng)
-        console.log("HERE IS THE ADDRESS AGAIN:", address)
         yield put({ type: FETCH_WEATHER_DATA_SUCCESS, data })
         yield put({ type: SET_LOCATION, location: address })
-        console.log("Waiting for form height animation")
+
         yield startFormHeightAnimation()
         yield take(ANIMATE_FORM_HEIGHT_END)
-        yield endFormHeightAnimation()
-        console.log("form height animation finished")
-    
+        yield endFormHeightAnimation()    
     } catch(e) {
         console.log(e)
         yield put({type: FETCH_WEATHER_DATA_ERROR})        
